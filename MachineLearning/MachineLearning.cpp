f7e8275e98e392efeb436218d312706228d7c29b
Virbek <61600649+Virbek@users.noreply.github.com>
2025-02-02 14:21:46 +0100

    ajout de la fenetre d'affichage algoLin

--- MachineLearning/MachineLearning.cpp
+++ MachineLearning/MachineLearning.cpp
@@ -1,12 +1,11 @@
 // MachineLearning.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
 //
-
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+//int main()
+//{
+//    std::cout << "Hello World!\n";
+//}
 
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
 // Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
